11b035d63f048b649b8b70a6a4b456b2826ce8e9
miyaz <shi_miyazato_r@dreamarts.co.jp>
2015-11-06 10:08:22 +0900

    コマンドの入力（スペース数など）チェックを緩和

--- lib/ruboty/ec2/version.rb
+++ lib/ruboty/ec2/version.rb
@@ -1,5 +1,5 @@
 module Ruboty
   module Ec2
-    VERSION = "0.4.3"
+    VERSION = "0.4.4"
   end
 end
--- lib/ruboty/handlers/ec2.rb
+++ lib/ruboty/handlers/ec2.rb
@@ -21,32 +21,32 @@ module Ruboty
   module Handlers
     class Ec2 < Base
       # 自動起動／停止系
-      on(/ec2 autostart (?<cmd>exec|list|add|del) *(?<ins_name>\S+)*\z/,
-                                           name: 'autostart', description: 'manage auto-start instances')
-      on(/ec2 autostop (?<cmd>exec|list|add|del) *(?<ins_name>\S+)*\z/,
-                                           name: 'autostop',  description: 'manage auto-stop instances')
+      on(/ec2 *autostart +(?<cmd>exec|list|add|del) *(?<ins_name>\S+)*\z/,
+                                              name: 'autostart', description: 'manage auto-start instances')
+      on(/ec2 *autostop +(?<cmd>exec|list|add|del) *(?<ins_name>\S+)*\z/,
+                                              name: 'autostop',  description: 'manage auto-stop instances')
 
       # インスタンス操作系
-      on(/ec2 create (?<ins_name>\S+) *(?<ami_id>\S+)*\z/,
-                                           name: 'create',    description: 'create instance')
-      on /ec2 stop (?<ins_name>\S+)\z/,    name: 'stop',      description: 'stop instance'
-      on /ec2 start (?<ins_name>\S+)\z/,   name: 'start',     description: 'start instance'
-      on /ec2 destroy (?<ins_name>\S+)\z/, name: 'destroy',   description: 'destroy instance'
-      on /ec2 archive *(?<ins_name>\S+)*\z/, name: 'archive', description: 'archive instance'
-      on /ec2 extract (?<ins_name>\S+)\z/, name: 'extract',   description: 'extract backed up instance'
-      on /ec2 copy (?<from_arc>\S+) +(?<to_ins>\S+)\z/,
-                                           name: 'copy',      description: 'copy instance'
-      on /ec2 access (?<cmd>give|revoke) +(?<ins_name>\S+) *(?<sg_name>\S+)*\z/,
-                                           name: 'access',    description: 'manage permitted source ip'
+      on(/ec2 *create +(?<ins_name>\S+) *(?<ami_id>\S+)*\z/,
+                                              name: 'create',    description: 'create instance')
+      on /ec2 *stop +(?<ins_name>\S+)\z/,     name: 'stop',      description: 'stop instance'
+      on /ec2 *start +(?<ins_name>\S+)\z/,    name: 'start',     description: 'start instance'
+      on /ec2 *destroy +(?<ins_name>\S+)\z/,  name: 'destroy',   description: 'destroy instance'
+      on /ec2 *archive *(?<ins_name>\S+)*\z/, name: 'archive', description: 'archive instance'
+      on /ec2 *extract +(?<ins_name>\S+)\z/,  name: 'extract',   description: 'extract backed up instance'
+      on /ec2 *copy +(?<from_arc>\S+) +(?<to_ins>\S+)\z/,
+                                              name: 'copy',      description: 'copy instance'
+      on /ec2 *access +(?<cmd>give|revoke) +(?<ins_name>\S+) *(?<sg_name>\S+)*\z/,
+                                              name: 'access',    description: 'manage permitted source ip'
 
       # インスタンスメタ情報管理系
-      on /ec2 detail (?<ins_name>\S+)\z/,  name: 'detail',    description: 'show instance/archive/AMI detail information'
-      on(/ec2 list *((?<resource>instance|archive|ami)*|(?<resource>filter) +(?<word>\S+)+) *\z/,
-                                           name: 'list',      description: 'show instance/archive/AMI list')
-      on(/ec2 usage *(?<yyyymm>last|20\d{4}+)*\z/,
-                                           name: 'usage',     description: 'show instance usage of specified month')
-      on(/ec2 edit (?<tag_name>spec|desc|param) +(?<ins_name>\S+) +(?<data>.+)\z/m,
-                                           name: 'edit',      description: 'edit data of spec, desc and param')
+      on /ec2 *detail +(?<ins_name>\S+)\z/,   name: 'detail',    description: 'show instance/archive/AMI detail information'
+      on(/ec2 *list *((?<resource>instance|archive|ami)*|(?<resource>filter) +(?<word>\S+)+) *\z/,
+                                              name: 'list',      description: 'show instance/archive/AMI list')
+      on(/ec2 *usage *(?<yyyymm>last|20\d{4}+)*\z/,
+                                              name: 'usage',     description: 'show instance usage of specified month')
+      on(/ec2 *edit +(?<tag_name>spec|desc|param) +(?<ins_name>\S+) +(?<data>.+)\z/m,
+                                              name: 'edit',      description: 'edit data of spec, desc and param')
 
       def create(message)
         Ruboty::Ec2::Actions::Create.new(message).call
